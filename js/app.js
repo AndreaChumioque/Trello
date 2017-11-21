2996df6f30937da7d64a9dac05827306e78ee3f7
andrea-isabel <andrea19.93.15@gmail.com>
2017-11-21 11:56:06 -0500

    Optimizar codigo

--- js/app.js
+++ js/app.js
@@ -1,71 +1,61 @@
-window.onload = function() {
-  var addListBox = document.getElementById('add-list-box');
+window.addEventListener('load', begin);
+
+function begin() {
+  var container = document.getElementById('lists-container');
+  var addList = document.getElementById('add-list');
+  var listForm = document.getElementById('list-form');
+  var listInput = document.getElementById('list-input');
   var saveBtn = document.getElementById('save-btn');
   var closeListBtn = document.getElementById('close-list-btn');
-  var newListName = document.getElementById('add-list-input');
-  var container = document.getElementById('lists-container');  
 
-  document.addEventListener('click', expandBox);
-  window.addEventListener('click', collapseAddListBox);
-  saveBtn.addEventListener('click', addList);
+  document.addEventListener('click', expandForm);
+  window.addEventListener('click', collapseForm);
+  saveBtn.addEventListener('click', newList);
   document.addEventListener('click', addTicket);
 
   // Función para expandir elemento inicial
-  function expandBox(event) {
-    if (event.target.className === 'collapsed-list-box' || event.target.parentElement.className === 'collapsed-list-box') {
-      addListBox.classList.replace('collapsed-list-box', 'expanded-box');
-      changeDisplay('hidden', 'show', addListBox);
-      newListName.focus();
-    }
-
-    if (event.target.classList.contains('collapsed-ticket-box') || event.target.parentElement.classList.contains('collapsed-ticket-box')) {
-      var addTicketBox = event.target.parentElement;
-      addTicketBox.classList.replace('collapsed-ticket-box', 'expanded-box');
-      changeDisplay('hidden', 'show', addTicketBox);
-      addTicketBox.lastChild.firstChild.focus();
+  // WORKS FINE FOR LIST
+  function expandForm(event) {
+    if (event.target === addList) {
+      event.target.classList.toggle('hidden');
+      listForm.classList.toggle('hidden');
+      listInput.focus();
     }
   }
 
   // Función para colapsar al hacer click fuera
-  function collapseAddListBox(event) {
-    if ((event.target !== addListBox && event.target.closest('div') !== addListBox) || event.target === closeListBtn) {
-      changeDisplay('show', 'hidden', addListBox);
-      addListBox.classList.replace('expanded-box', 'collapsed-list-box');
-    }
-  }
-
-  // Función para ocultar y mostrar elementos
-  function changeDisplay(display1, display2, mainElement) {
-    debugger;
-    if (mainElement.classList.contains('expanded-box') || mainElement.lastChild.classList.contains('expanded-box')) {
-      var elements = mainElement.getElementsByClassName(display1);
-      for (var i = 0; i < elements.length; i)
-        elements[i].classList.replace(display1, display2);
-      mainElement.firstElementChild.className = display1;
+  // WORKS FINE FOR LIST
+  function collapseForm(event) {
+    if ((event.target !== addList && event.target !== listForm && event.target.closest('form') !== listForm) || event.target === closeListBtn) {
+      addList.className = '';
+      listForm.className = 'hidden';
     }
   }
 
   // Función para agregar nueva lista (solo si se ingresa un nombre de lista) boton Guardar
-  function addList(event) {
+  // THIS FUNCTION WORKS FINE
+  function newList(event) {
     event.preventDefault();
-    if (event.target.matches('button') && newListName.value !== '') {
+
+    if (event.target.matches('button') && listInput.value) {
       var newListBox = document.createElement('div');
       newListBox.classList.add('new-list');
       var newListTitle = document.createElement('p');
-      newListTitle.textContent = newListName.value;
+      newListTitle.textContent = listInput.value;
       newListTitle.classList.add('list-title');
 
-      var addCardBox = createAddCardBox();
+      var addCardBox = createTicketBox();
       newListBox.appendChild(newListTitle);
       newListBox.appendChild(addCardBox);
-      container.insertBefore(newListBox, addListBox);
-      newListName.value = '';
+      container.insertBefore(newListBox, addList.parentElement);
+      listInput.value = '';
     }
-    newListName.focus();
+    listInput.focus();
   }
 
   // Crear addCardBox
-  function createAddCardBox() {
+  // THIS FUNCTION WORKS FINE
+  function createTicketBox() {
     // Crear elementos y contenidos de texto
     var box = document.createElement('div');
     var text = document.createElement('p');
@@ -78,10 +68,9 @@ window.onload = function() {
     var btnClose = document.createElement('button');
 
     // Asignar clases
-    box.classList.add('add-ticket-box', 'collapsed-ticket-box');
-    input.classList.add('add-ticket-input', 'add-input', 'hidden');
-    btnAdd.classList.add('add-btn', 'green-btn', 'hidden');
-    btnClose.classList.add('close-ticket-button', 'close-btn', 'icon-x', 'hidden');
+    text.classList.add('add-ticket');
+    form.classList.add('hidden');
+    btnClose.classList.add('close-ticket-button', 'icon-x');
 
     // Añadirlos al documento
     form.appendChild(input);
@@ -94,8 +83,8 @@ window.onload = function() {
   }
 
   // Función para añadir nueva tarjeta
+  // THIS FUNCTION WORKS FINE
   function addTicket(event) {
-    debugger;
     event.preventDefault();
     if (event.target.matches('.add-btn', '.green-btn', '.show')) {
       var box = event.target.parentElement.parentElement;
@@ -112,3 +101,15 @@ window.onload = function() {
     }
   }
 };
+
+// Función para ocultar y mostrar elementos
+// function changeDisplay(display1, display2, mainElement) {
+//   debugger;
+//   if (mainElement.classList.contains('expanded-box') || mainElement.lastChild.classList.contains('expanded-box')) {
+//     // var elements = mainElement.getElementsByClassName(display1);
+//     // for (var i = 0; i < elements.length; i)
+//     //   elements[i].classList.replace(display1, display2);
+//     // mainElement.firstElementChild.className = display1;
+
+//   }
+// }
